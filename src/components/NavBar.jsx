4750aaf24bce988cc9c4520a4c46acd31be82586
Ellyzes John David <ellyzesjohn@gmail.com>
2025-08-09 13:17:41 +0800

    update

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom'
 function NavBar() {
   return (
     <nav className={styles.NavContainer}>
-        <h1 className={styles.Logo}>Logo</h1>
+        <h1 className={styles.Logo}>EJ</h1>
         <ul className={styles.ListContainer}>
             <Link to='/' className={styles.link}><li className={styles.li}>Tasks</li></Link>
             <Link to='/complete' className={styles.link}><li className={styles.li}>Complete List</li></Link>
